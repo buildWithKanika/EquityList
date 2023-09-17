b60a846460c90601d6e211c59ab84242cdeab923
kanika kala <kala.kanika1998@gmail.com>
2023-09-17 14:38:03 +0530

    new commit

--- src/service/mockApi.js
+++ src/service/mockApi.js
@@ -1,7 +1,7 @@
 async function fetchLedgerData(user){
     try{
         //const ledgerData = await import('../Data/simple_ledger.json');
-        const ledgerData = require('../Data/complicated_ledger.json');
+        const ledgerData = require('../Data/simple_legder.json');
         // read the file from data folder
 
         await new Promise(resolve => setTimeout(resolve, 1000));
